32e8f1e6587182308dbf07bcc0728cfa7a37032a
Iggyfufu <kennethzhu07@gmail.com>
2018-04-24 12:51:21 -0400

    added ls pwd

---
+++ bash.js
@@ -0,0 +1,19 @@
+//Output a prompt
+const pwd = require('./pwd');
+const ls = require('./ls');
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === 'pwd') {
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
+  }
+
+
+  process.stdout.write('\nYou typed: ' + cmd);
+  process.stdout.write('\nprompt >'); //adds prompt to ls output weird...
+});
